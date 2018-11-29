a2fdcc90e242bb2321428e73d95568e18478e7de
krzysiekj94 <k.jerzynski1@gmail.com>
2018-11-29 22:25:33 +0100

    - test, compare algorithm results with calculation from laboratory

--- java/SearchEngine.java
+++ java/SearchEngine.java
@@ -4,8 +4,8 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class SearchEngine {
-    private static String DOCUMENTS_PATH = "documents.txt";
-    private static String KEYWORDS_PATH = "keywords.txt";
+    private static String DOCUMENTS_PATH = "documents.txt"; // = "documents-lab3.txt";
+    private static String KEYWORDS_PATH = "keywords.txt"; 	// = "keywords-lab3.txt";
     private static int MAX_PRINT_RESULTS = 10;
     private List<Document> _documents;
     private Dictionary _dictionary;
